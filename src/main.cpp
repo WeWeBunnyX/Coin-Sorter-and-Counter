f1a3e51cb3850b7b6ecdd586c1f161b9112600c3
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-12-27 17:09:35 +0500

    Test all TCRT5000 IR sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,77 @@
 #include <Wire.h>
 #include <LCDI2C_Multilingual.h>
 
-LCDI2C_Latin lcd(0x27, 16, 2);  
+LCDI2C_Latin lcd(0x27, 16, 2);
+
+const int sensor1DigitalPin = 12;
+const int sensor1AnalogPin = A0;
+
+const int sensor2DigitalPin = 11;
+const int sensor2AnalogPin = A1;
+
+const int sensor3DigitalPin = 3;
+const int sensor3AnalogPin = A2;
+
+const int sensor4DigitalPin = 8;
+const int sensor4AnalogPin = A3;
 
 void setup() {
-  lcd.init();       
-  lcd.backlight();   
-  
-  lcd.command(0x0C);  
+  lcd.init();      
+  lcd.backlight();  
 
-  lcd.clear();  
+  lcd.clear();   
+  lcd.setCursor(0, 0);  
+  lcd.print("12345");   
+
+  Serial.begin(9600);
+
+  pinMode(sensor1DigitalPin, INPUT);
+  pinMode(sensor1AnalogPin, INPUT);
+
+  pinMode(sensor2DigitalPin, INPUT);
+  pinMode(sensor2AnalogPin, INPUT);
+
+  pinMode(sensor3DigitalPin, INPUT);
+  pinMode(sensor3AnalogPin, INPUT);
+
+  pinMode(sensor4DigitalPin, INPUT);
+  pinMode(sensor4AnalogPin, INPUT);
 }
 
 void loop() {
-  for (int i = 1; i <= 100; i++) {
-    lcd.clear();   
-    lcd.setCursor(0, 0);   
-    lcd.print(i);   
+  // Read sensor values
+  int sensor1DigitalValue = digitalRead(sensor1DigitalPin);
+  int sensor1AnalogValue = analogRead(sensor1AnalogPin);
 
-    delay(500);  
-  }
+  int sensor2DigitalValue = digitalRead(sensor2DigitalPin);
+  int sensor2AnalogValue = analogRead(sensor2AnalogPin);
+
+  int sensor3DigitalValue = digitalRead(sensor3DigitalPin);
+  int sensor3AnalogValue = analogRead(sensor3AnalogPin);
+
+  int sensor4DigitalValue = digitalRead(sensor4DigitalPin);
+  int sensor4AnalogValue = analogRead(sensor4AnalogPin);
+
+ 
+  Serial.print("Sensor 1 - D: ");
+  Serial.print(sensor1DigitalValue);
+  Serial.print(", A: ");
+  Serial.print(sensor1AnalogValue);
+  Serial.print(" | Sensor 2 - D: ");
+  Serial.print(sensor2DigitalValue);
+  Serial.print(", A: ");
+  Serial.print(sensor2AnalogValue);
+  Serial.print(" | Sensor 3 - D: ");
+  Serial.print(sensor3DigitalValue);
+  Serial.print(", A: ");
+  Serial.print(sensor3AnalogValue);
+  Serial.print(" | Sensor 4 - D: ");
+  Serial.print(sensor4DigitalValue);
+  Serial.print(", A: ");
+  Serial.print(sensor4AnalogValue);
+  Serial.print("\r");  
+
+  delay(1000);  
 }
+
+
