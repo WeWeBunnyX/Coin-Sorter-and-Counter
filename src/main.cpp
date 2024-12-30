854da6ba9ce73b7dbe032d61bb260680bd4f6b20
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-12-30 23:05:51 +0500

    Test with digital values

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,17 @@
 LCDI2C_Latin lcd(0x27, 16, 2);
 
 const int sensorDigitalPins[] = {12, 11, 3, 8};
-const int sensorAnalogPins[] = {A0, A1, A2, A3};
 const int numSensors = 4;
 
 volatile int count = 0; 
 volatile unsigned long lastDetectionTime = 0;
-const unsigned long debounceDelay = 0.5; // Reduced debounce delay for faster detection
+const unsigned long debounceDelay = 0.1; // Reduced debounce delay for faster detection
 
 void countObject() {
   unsigned long currentTime = millis();
   if (currentTime - lastDetectionTime > debounceDelay) {
-    count++;
-    lastDetectionTime = currentTime;
-    Serial.print("Count: ");
-    Serial.println(count);
+	count++;
+	lastDetectionTime = currentTime;
   }
 }
 
@@ -44,19 +41,12 @@ void updateLCD() {
   lcd.print(count);
 }
 
-void checkAnalogSensors() {
-  int sensor1AnalogValue = analogRead(sensorAnalogPins[0]);          // Sensor 1 
-  if (sensor1AnalogValue < 610) {
-    count++;
-    updateLCD();
-    delay(10); // Small delay to avoid multiple increments for the same detection
-  }
-
-  int sensor2AnalogValue = analogRead(sensorAnalogPins[1]);            //Sensor 2
-  if (sensor2AnalogValue < 580) {
-    count++;
-    updateLCD();
-    delay(10); // Small delay to avoid multiple increments for the same detection
+void checkDigitalSensors() {
+  for (int i = 0; i < numSensors; i++) {
+    int sensorDigitalValue = digitalRead(sensorDigitalPins[i]);
+    if (sensorDigitalValue == LOW) {
+      countObject();
+    }
   }
 }
 
@@ -64,14 +54,11 @@ void printSensorStates() {
   Serial.print("\r"); 
   for (int i = 0; i < numSensors; i++) {
     int sensorDigitalValue = digitalRead(sensorDigitalPins[i]);
-    int sensorAnalogValue = analogRead(sensorAnalogPins[i]);
   
     Serial.print("Sensor ");
     Serial.print(i + 1);
     Serial.print(" - D: ");
     Serial.print(sensorDigitalValue);
-    Serial.print(", A: ");
-    Serial.print(sensorAnalogValue);
     Serial.print(" | ");
   }
   Serial.print("Count: ");
@@ -86,7 +73,7 @@ void setup() {
 
 void loop() {
   updateLCD();
-  checkAnalogSensors();
+  checkDigitalSensors();
   printSensorStates();
   delay(1); // Small delay for display updates
 }
